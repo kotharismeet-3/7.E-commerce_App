931d38ffe287143d12e3ca2f402c07c95d5b319f
kotharismeet-3 <91626706+kotharismeet-3@users.noreply.github.com>
2021-11-23 18:09:20 +0530

    Product Server Running at 5000

--- backend/products.js
+++ backend/products.js
@@ -1,43 +1,16 @@
+const express = require( 'express');
 const dotenv = require( 'dotenv');
 const cors = require('cors');
-const express = require( 'express');
-
 const connectDB = require( './models/mongo_db.js');
-
-const session = require('express-session');
-const passport = require('passport');
-
-const {createProxyMiddleware} = require('http-proxy-middleware');
 
 dotenv.config();
 connectDB(); 
 
-require( './services/passport')(passport);
-
-const app = express();
-app.use(
-    session({
-        secret: process.env.cookieKey,
-        resave: false,
-        saveUninitialized: false
-    })
-);
-
 app.use(cors());
 app.use(express.json());
-
-app.use(passport.initialize());
-app.use(passport.session());
-require('./routes/authRoutes.js')(app);
-
-require('./routes/productRoutes.js')(app);
 const port = 5000;
 
-app.use(createProxyMiddleware('/', { 
-    target: 'http://localhost:3000/',
-    changeOrigin: true 
-    }
-));
+require('./routes/productRoutes.js')(app);
 
 app.use((err, req, res, next)=> {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
